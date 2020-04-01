57654b77a78af7c8a457f9c60d4f694d2729819e
Aaron Culp <aaron_culp@anfcorp.com>
2020-04-01 13:20:27 -0400

    created and finished fetchPost action. Formatted it and commented out old ugly function to see the steps. Also added in separate reducer file..postsReducer and imported in index inside reducer folder. In main index folder you will see an added applyMiddleware variable to connect the middleware for fetch calls.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-    // Bad approach ... breaking rules of ACTION creator
-    const response = await jsonPlaceholder.get('/posts')
+// same function as commented out below just cleaner.
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POST", payload: response });
+};
 
-    return {
-        type: 'FETCH_POST',
-        payload: response
-    }
-}
+// with Redux Thunk we can MANUALLY dispatch an Action at any time.
+
+// export const fetchPosts = () => {
+//   return async dispatch => {
+//     const response = await jsonPlaceholder.get("/posts");
+
+//     dispatch({ type: 'FETCH_POST', payload: response })
+//   };
+// };
